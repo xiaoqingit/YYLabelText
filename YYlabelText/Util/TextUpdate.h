a01975b0f25c707098e359db3e1904d50d28c454
xiaoqingit <451852869@qq.com>
2018-06-06 16:33:26 +0800

    测试更新

---
+++ YYlabelText/Util/TextUpdate.h
@@ -0,0 +1,13 @@
+//
+//  TextUpdate.h
+//  YYlabelText
+//
+//  Created by oprah on 2018/6/6.
+//  Copyright © 2018年 oprah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextUpdate : NSObject
+
+@end
---
+++ YYlabelText/Util/TextUpdate.m
@@ -0,0 +1,13 @@
+//
+//  TextUpdate.m
+//  YYlabelText
+//
+//  Created by oprah on 2018/6/6.
+//  Copyright © 2018年 oprah. All rights reserved.
+//
+
+#import "TextUpdate.h"
+
+@implementation TextUpdate
+
+@end
